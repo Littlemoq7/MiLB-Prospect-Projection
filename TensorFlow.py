2c05f8c3d03ed8aa19483506bc3588680ba2af72
Elliott Miller <57118301+NinjaPuffE@users.noreply.github.com>
2025-01-03 16:09:43 -0500

    changes

--- TensorFlow.py
+++ TensorFlow.py
@@ -13,7 +13,7 @@ data = pd.read_csv('mergedOutput.csv')
 X = data[['PA', 'BB%', 'K%', 'ISO', 'GB%', 'wRC+']].values
 y = data['category'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
@@ -39,4 +39,8 @@ history = model.fit(X_train, y_train, epochs=50, batch_size=8, validation_data=(
 test_loss, test_accuracy = model.evaluate(X_test, y_test)
 print(f"Test Accuracy: {test_accuracy:.2f}")
 
-
+new_player = [[230,0.1,0.29130434699999996,0.14492753600000002,0.449275362,114.00819401335193]]  # Example stats for a new player
+new_player = scaler.transform(new_player)
+prediction = model.predict(new_player)
+predicted_category = prediction.argmax() + 1
+print(f"Predicted Category: {predicted_category}")
